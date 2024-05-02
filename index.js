ae516176e033f5dfb3472f2a7cdd4aeed2278d84
hamimDev <mdhamimhowladerasif@gmail.com>
2024-05-02 08:14:49 +0600

    add many gate and post operation

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 // main
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0epcy0w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,12 +26,41 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const chocolateCollection = client.db("chocolate").collection("chocolate");
+    const carDoctorCollection = client
+      .db("car-doctor-server")
+      .collection("services");
 
-    app.post("/chocolate", async (req, res) => {
-      const newChocolate = req.body;
-      console.log(newChocolate);
-      const result = await chocolateCollection.insertOne(newChocolate);
+    const BookingCollection = client
+      .db("car-doctor-server")
+      .collection("bookings");
+
+    app.get("/services", async (req, res) => {
+      const cursor = carDoctorCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await carDoctorCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/bookings", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await BookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await BookingCollection.insertOne(booking);
       res.send(result);
     });
 
@@ -49,8 +77,8 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Coffee-store-server-Running");
+  res.send("Car Doctor Server Is Running");
 });
 app.listen(port, () => {
-  console.log(`coffee-server-is-running-on-PORT: ${port}`);
+  console.log(`Car Doctor Server Is Running-on-PORT: ${port}`);
 });
